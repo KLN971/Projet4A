badda49b5bbf5d0440cee8a1dba5939be001bfe7
KLN971 <Klautric@gmail.com>
2023-10-25 15:57:10 +0200

    Initial commit

---
+++ src/main/java/com/example/projet4a/Projet4AApplication.java
@@ -0,0 +1,13 @@
+package com.example.projet4a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet4AApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet4AApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projet4a/Projet4AApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet4a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projet4AApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
